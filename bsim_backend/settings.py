f756f8d585ee88a99fd42082d4a4471297433ce2
Muhammad Ahmad <muhammadahmadbwp@hotmail.com>
2022-05-02 04:49:36 +0500

    static files settings

--- bsim_backend/settings.py
+++ bsim_backend/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 if DEBUG:
     STATICFILES_DIRS = [
@@ -141,7 +141,7 @@ if DEBUG:
 else:
     STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "media/"
 
 MEDIA_ROOT = BASE_DIR / "media"
 
